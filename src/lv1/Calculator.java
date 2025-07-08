99f5557c447eddeb64afc997a79329906936a120
joon448 <joonjdy448@gmail.com>
2025-07-08 12:20:31 +0900

    Feat: ëAdd ㅡmain loop with exit condition

--- src/lv1/Calculator.java
+++ src/lv1/Calculator.java
@@ -6,45 +6,56 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 양의 정수 입력
-        System.out.print("첫 번째 숫자를 입력하세요 (0 이상의 정수): ");
-        int num1 = scanner.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요 (0 이상의 정수): ");
-        int num2 = scanner.nextInt();
-        //개행 문자 처리
-        scanner.nextLine();
 
-        //사칙연산 기호 입력
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = scanner.nextLine().charAt(0);
+        while(true) {
+            // 양의 정수 입력
+            System.out.print("첫 번째 숫자를 입력하세요 (0 이상의 정수): ");
+            int num1 = scanner.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요 (0 이상의 정수): ");
+            int num2 = scanner.nextInt();
+            //개행 문자 처리
+            scanner.nextLine();
 
-        //연산
-        int result = 0;
-        switch (operator){
-            case '+':
-                result = num1 + num2;
+            //사칙연산 기호 입력
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = scanner.nextLine().charAt(0);
+
+            //연산
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    // 나눗셈 분모 0 예외 처리
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                        scanner.close();
+                        System.exit(0);
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("잘못된 입력입니다.");
+                    continue;
+            }
+            System.out.println(result);
+
+            //반복 종료 조건
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String status = scanner.nextLine();
+            if (status.equals("exit")) {
+                System.out.println("종료되었습니다.");
                 break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                // 나눗셈 분모 0 예외 처리
-                if (num2 == 0){
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    scanner.close();
-                    System.exit(0);
-                }
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("잘못된 입력입니다.");
-                scanner.close();
-                System.exit(0);
+            }
         }
-        System.out.printf("%d %c %d = %d", num1, operator, num2, result);
         scanner.close();
+
     }
 }
