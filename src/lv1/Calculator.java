90be25276352eea9902d88b3c2ebd956b58b5551
joon448 <joonjdy448@gmail.com>
2025-07-08 11:35:40 +0900

    Feat: 사칙연산 기호 input 기능 추가

--- src/lv1/Calculator.java
+++ src/lv1/Calculator.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // 양의 정수 입력
         System.out.print("첫 번째 숫자를 입력하세요 (0 이상의 정수): ");
         int num1 = scanner.nextInt();
         System.out.print("두 번째 숫자를 입력하세요 (0 이상의 정수): ");
         int num2 = scanner.nextInt();
+        //개행 문자 처리
+        scanner.nextLine();
+
+        //사칙연산 기호 입력
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = scanner.nextLine().charAt(0);
         scanner.close();
     }
 }
