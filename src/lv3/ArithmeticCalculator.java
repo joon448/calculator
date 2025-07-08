6f43c58939f6d31237f541c84aa27973084a04f3
joon448 <joonjdy448@gmail.com>
2025-07-08 19:15:25 +0900

    Feat: Implement ArithmeticCalculator using OperatorType enum

--- src/lv3/ArithmeticCalculator.java
+++ src/lv3/ArithmeticCalculator.java
@@ -2,43 +2,46 @@ package lv3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class Calculator {
-    private List<Integer> results =  new ArrayList<Integer>();
+public class ArithmeticCalculator<T extends Number> {
+    private List<Double> results =  new ArrayList<>();
 
-    public Integer calculate(int num1, int num2, char operator){
-        int result = 0;
+    public Optional<Double> calculate(T n1, T n2, OperatorType operator){
+        double num1 = n1.doubleValue();
+        double num2 = n2.doubleValue();
+        double result;
         switch (operator) {
-            case '+':
+            case ADD:
                 result = num1 + num2;
                 break;
-            case '-':
+            case SUB:
                 result = num1 - num2;
                 break;
-            case '*':
+            case MUL:
                 result = num1 * num2;
                 break;
-            case '/':
+            case DIV:
                 // 나눗셈 분모 0 예외 처리
                 if (num2 == 0) {
                     System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    return null;
+                    return Optional.empty();
                 }
                 result = num1 / num2;
                 break;
             default:
-                System.out.println("잘못된 입력입니다.");
+                System.out.println("잘못된 연산입니다.");
                 return null;
         }
         results.add(result);
-        return result;
+        return Optional.of(result);
     }
 
-    public List<Integer> getResults() {
+    public List<Double> getResults() {
         return results;
     }
 
-    public void setResults(List<Integer> results) {
+    public void setResults(List<Double> results) {
         this.results = results;
     }
 
